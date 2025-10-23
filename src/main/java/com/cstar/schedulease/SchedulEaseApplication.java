32155bd688fa417bb784e3f41760006a4c5d18f5
clarkyang <k345974523@gmail.com>
2025-10-23 19:51:16 -0400

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/cstar/schedulease/SchedulEaseApplication.java
@@ -0,0 +1,13 @@
+package com.cstar.schedulease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulEaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulEaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cstar/schedulease/SchedulEaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cstar.schedulease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulEaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
